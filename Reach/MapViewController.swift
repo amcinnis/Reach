d5aca81858708ab7801e5fd9cf7bed22721d991b
Austin McInnis <austin.mcinnis@icloud.com>
2017-05-09 14:20:10 -0700

    Switching to CSL iMac

---
+++ Reach/MapViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MapViewController.swift
+//  Reach
+//
+//  Created by Austin McInnis on 5/4/17.
+//  Copyright © 2017 Austin McInnis. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let initialLocation = CLLocation(latitude: 35.3050, longitude: -120.6625)
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(initialLocation.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+    }
+
+}
